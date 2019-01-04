c4d6c8bc8b467837f78223e1b0a941630edaf94e
Chanda Grayson <chandagrayson@gmail.com>
2019-01-03 22:19:50 -0500

    I failed

--- app/models/RecipeCard.rb
+++ app/models/RecipeCard.rb
@@ -1,9 +1,14 @@
 class RecipeCard
 
+  attr_reader :user, :recipe, :rating, :date
+
   @@all = []
 
-  def initialize(name)
-    @name = name
+  def initialize(user, recipe, rating, date)
+    @user = user
+    @recipe = recipe
+    @rating = rating 
+    @date = date
     @@all << self
   end
 
@@ -13,33 +18,34 @@ class RecipeCard
 
   # - `RecipeCard#date`
   # should return the date of the entry
-  def date
-    self.date
-  end
+  # def date
+  #   self.date
+  # end
 
                     # - `RecipeCard#rating`
                     # should return the rating (an integer) a user has given their entry
-  def rating
-    User.all.select do |recipes|
-      recipes.rating == self
-    end
-  end
+  # def rating
+  #   User.all.select do |recipes|
+  #     recipes.rating == self
+  #   end
+  # end
 
                     # - `RecipeCard#user`
                     # should return the user to which the entry belongs
-  def user
-    User.all.select do |entry|
-    entry.user == self
-    end
-  end
+  # def user
+  #   @user
+    # User.all.select do |entry|
+    # entry.user == self
+    # end
+  # end
 
                     # - `RecipeCard#recipe`
                     # should return the recipe to which the entry belongs
-  def recipe
-    Recipe.all.select do |entry|
-      entry.recipe == self
-    end
-  end
+  # def recipe
+  #   Recipe.all.select do |entry|
+  #     entry.recipe == self
+  #   end
+  # end
 
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -8,6 +8,10 @@ nachos = Recipe.new("Nachos")
 taco = Recipe.new("Taco")
 enchilada = Recipe.new("Enchilada")
 
+emily_nachos = RecipeCard.new(emily, nachos, 100, "1/3/19")
+cat_taco = RecipeCard.new(cat, taco, 75, "1/3/19")
+ryan_enchilada = RecipeCard.new(ryan, enchilada, 60, "1/3/19")
+
 
 
 binding.pry
