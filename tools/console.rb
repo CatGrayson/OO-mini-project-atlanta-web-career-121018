0e1adaa488043fd1c4634a0bf58c9047ec10ab0d
Emily Wilder <emilywilder.earth@gmail.com>
2019-01-03 21:53:30 -0500

    add some things to play with

--- tools/console.rb
+++ tools/console.rb
@@ -1,3 +1,13 @@
 require_relative '../config/environment.rb'
 
+emily = User.new("Emily")
+cat = User.new("Cat")
+ryan = User.new("Ryan")
+
+nachos = Recipe.new("Nachos")
+taco = Recipe.new("Taco")
+enchilada = Recipe.new("Enchilada")
+
+
+
 binding.pry
